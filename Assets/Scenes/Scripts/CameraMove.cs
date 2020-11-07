27138d5ebf2d3390688631a882d44c0349f3a24d
alticator <main@alticator.com>
2020-11-07 09:53:43 +0200

    Version 2
    1. Updated Map
    2. Added Map
    3. Added Pause

--- Assets/Scenes/Scripts/CameraMove.cs
+++ Assets/Scenes/Scripts/CameraMove.cs
@@ -8,19 +8,46 @@ public class CameraMove : MonoBehaviour
     public Transform targetObject;
     public Vector3 offset;
     private Vector3 cameraPosition;
+    private bool mapView;
 
     public float followSpeed;
     public float rotationSpeed;
 
+    public int mapSize;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (mapView)
+            {
+                mapView = false;
+            }
+            else
+            {
+                mapView = true;
+            }
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        var targetPosition = targetObject.TransformPoint(offset);
-        cameraPosition = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
-        transform.position = cameraPosition;
+        if (!mapView)
+        {
+            var targetPosition = targetObject.TransformPoint(offset);
+            cameraPosition = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+            transform.position = cameraPosition;
 
-        var direction = targetObject.position - transform.position;
-        var rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+            var direction = targetObject.position - transform.position;
+            var rotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+        else if (mapView)
+        {
+            transform.position = Vector3.up * mapSize;
+            var rotation = new Vector3(90, 0, 0);
+            transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
---
+++ Assets/Scenes/Scripts/Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool paused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = 1;
+            }
+            else
+            {
+                paused = true;
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
